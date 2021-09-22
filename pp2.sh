ac091ed0a0d4dcbdc7875a9a57cc56a700f62237
MadhuriGadkari1993 <gadkarimg93@gmail.com>
2021-09-22 21:34:49 +0530

    seqencepracticeprblem

---
+++ pp2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+function roll_die() {
+
+# capture parameter
+declare -i DIE_SIDES=$1
+
+# check for die sides
+if [ ! $DIE_SIDES -gt 0 ]; then
+# default to 6
+DIE_SIDES=6
+fi
+
+# echo to screen
+echo $[ ( $RANDOM % $DIE_SIDES )  + 1 ]
+
+}
+
+roll_die 6
+roll_die 2
